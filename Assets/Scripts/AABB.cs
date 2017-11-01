1c3af59698e9a2516538d33ab7b7c37619c6be31
BlaneTorregrossa <blanet97@cox.net>
2017-11-01 17:55:24 -0500

    Start on Sort and Sweep

---
+++ Assets/Scripts/AABB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Arguments for overlap when taken in
+[CreateAssetMenu]
+public class AABB : ScriptableObject
+{
+    public Vector2 min;
+    public Vector2 max;
+}
+
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Utilities : MonoBehaviour
+{
+    public AABB box1;
+    public AABB box2;
+
+    private void Start()
+    {
+        TestOverlap(box1, box2);
+    }
+
+    // To test overlap of objects
+    public bool TestOverlap(AABB I, AABB J)
+    { 
+        if (J.min.x >= I.min.x && J.min.x <= I.max.x)
+        {
+            Debug.Log("Overlap A!");
+        }
+
+        if (I.min.y >= J.min.y && I.min.y <= J.max.y)
+        {
+            Debug.Log("Overlap B!");
+        }
+
+        //if (J != I)
+        //{
+        //    Debug.Log("Box Overlaps!");
+        //}
+
+        return true;
+    }
+
+}
