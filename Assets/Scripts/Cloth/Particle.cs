34e6bc5b57de4482718ea8d370169f9978171066
BlaneTorregrossa <blanet97@cox.net>
2017-11-29 17:55:38 -0600

    Working on spring behavior for cloth physics

---
+++ Assets/Scripts/Cloth/Particle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blane
+{
+
+    [System.Serializable]
+    public class Particle
+    {
+        public float mass;
+
+        public Vector3 velocity;
+        public Vector3 acceleration;
+        public Vector3 position;
+        public Vector3 force;
+
+        public void Initilize()
+        {
+            mass = 1;
+            velocity = new Vector3(0, 0, 0);
+            acceleration = new Vector3(0, 0, 0);
+            position = new Vector3(0, 0, 0);
+            force = new Vector3(0, -9.81f, 0);
+        }
+
+        public Vector3 Update(float deltaTime)
+        {
+            acceleration = force / mass;
+            velocity += acceleration * deltaTime;
+            position += velocity * deltaTime;
+
+            return position;
+        }
+    }
+}
---
+++ Assets/Scripts/Cloth/SpringBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringBehavior : MonoBehaviour
+{
+
+    public GameObject particleObject;
+    public GameObject particleAltObject;
+    public GameObject anchorObject;
+
+    public Blane.Particle _particle;
+    public Blane.Particle _particleAlt;
+    protected Vector3 anchorPos;
+    protected float springConstant;
+    protected float restLength;
+
+    private Vector3 PullPosition;
+
+    void Start()
+    {
+        restLength = 5;
+        springConstant = 3;
+        anchorPos = anchorObject.transform.position;
+
+        _particle.Initilize();
+        _particleAlt.Initilize();
+        _particle.position = new Vector3(0, 0, 0);
+        _particleAlt.position = new Vector3(3, 0, 0);
+        particleObject.transform.position = _particle.position;
+        particleAltObject.transform.position = _particleAlt.position;
+
+    }
+
+    // * Not how it should be 
+    void CheckParticle(Blane.Particle p1, Blane.Particle p2, float ks, float lo)
+    {
+        float tempValue = ks * lo;
+
+        if (p1.position.y < -lo)
+        {
+            PullPosition = p1.position;
+            p1.force = new Vector3(0, tempValue / 2, 0);
+        }
+
+        if (p1.position.y > PullPosition.y / 2)
+        {
+            p1.force = new Vector3(0, -9.81f, 0);
+        }
+
+    }
+
+    void Update()
+    {
+        _particle.Update(Time.deltaTime);
+        _particleAlt.Update(Time.deltaTime);
+        CheckParticle(_particle, _particleAlt, restLength, springConstant);
+        particleObject.transform.position = _particle.position;
+        particleAltObject.transform.position = _particleAlt.position;
+    }
+}
